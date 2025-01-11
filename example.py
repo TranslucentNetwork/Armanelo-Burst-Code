a05260b046c48d7ce13d0b73030f78dba4fa3094
Translucent Network <148647657+TranslucentNetwork@users.noreply.github.com>
2025-01-11 04:36:53 -0600

    Update example.py

--- example.py
+++ example.py
@@ -37,16 +37,3 @@ async def listdevs(interaction: discord.Interaction):
     await interaction.response.send_message('Prio 3 Command')
 
 await bot.run('token')
-
-
-#How to Contribute
-#As a contributor, your responsibility is to create the commands based on the specified functionality and submit them via a pull request.
-
-#I will handle all the necessary configuration, including:
-
-#Assigning the correct priority and access levels for each command.
-#Setting up the appropriate server-specific permissions and roles.
-#Integrating commands with the database to ensure they function as expected.
-#Simply ensure that the command is defined, and include any relevant parameters or functionality you want the command to have. Once you've done that, submit it as a pull request, and I will handle the rest.
-
-
