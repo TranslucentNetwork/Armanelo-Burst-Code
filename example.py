2315033c960430822c487fdf32be53d9de05dbbd
Translucent Network <148647657+TranslucentNetwork@users.noreply.github.com>
2025-01-12 17:47:50 -0600

    Update example.py

--- example.py
+++ example.py
@@ -27,8 +27,8 @@ async def listdevs(interaction: discord.Interaction):
   if str(interaction.user.id) in devs or ops or service:
     await interaction.response.send_message('Prio 2 Command') 
 
-@bot.tree.command(name="priotwo", description="Prio 2 Command")
-@app_commands.guilds(discord.Object(id=main), *[discord.Object(id=guild_id) for guild_id in subserver],*[discord.Object(id=guild_id) for guild_id in miscservers]))
+@bot.tree.command(name="priothree", description="Prio 3 Command")
+@app_commands.guilds(discord.Object(id=main), *[discord.Object(id=guild_id) for guild_id in subserver], *[discord.Object(id=guild_id) for guild_id in miscservers]))
 async def listdevs(interaction: discord.Interaction):
   devs = await getdevs()
   ops = await getops()
